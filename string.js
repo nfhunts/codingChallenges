0df274956f41d0317309345eaa99b6bfc8551d10
nfhunts <nfhunts@gmail.com>
2019-12-11 13:38:50 -0700

    had to update some sytax errors

--- string.js
+++ string.js
@@ -1,9 +1,7 @@
 const toCapital = str => {
-    const words = str.split(" ")
-    return words.map(word => word[0].toUpperCase() + word.slice(1).join(" "))
-}
-
-const shortStr = "Hi there";
-const longStr = "the quick brown fox jumped over the lazy dog";
-
-toCapital(longStr);
+    const words = str.split(" ");
+    return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+  };
+  
+  const shortStr = "Hi there";
+  const longStr = "the quick brown fox jumped over the lazy dog";
